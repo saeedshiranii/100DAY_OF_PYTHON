0a9f0b10527017db9f161c31562f868dabaf444b
saeedshiranii <77902443+saeedshiranii@users.noreply.github.com>
2021-10-25 22:35:07 +0330

    DAY1 REFACTORE.

--- DAY1/main.py
+++ DAY1/main.py
@@ -2,4 +2,4 @@
 print("welcome to the band name generator.")
 city = input("what is name of the city you grew up? ""\n")
 pet = input("what is your pet's name? ""\n")
-print("your band name could be "+city+" "+pet)
+print(f"your band name could be {city}{pet}")
