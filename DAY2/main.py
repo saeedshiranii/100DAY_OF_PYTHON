bd3c4d2d6f820c29785eeb61cfcf0ae6e0135be5
saeedshiranii <77902443+saeedshiranii@users.noreply.github.com>
2021-10-26 23:59:45 +0330

    DAY REFACTOR.

--- DAY2/main.py
+++ DAY2/main.py
@@ -1,14 +1,17 @@
 # a tip calculator
 
+def tip():
+    """ formula = (bill/per)*1.percent"""
+    """ 1.percent is for paying money of food and tip (1 for food) and (0.tip  for tip) """ \
+            calculate = ((percent / 100) + 1)
+    pay = (bill / per) * calculate
+    pay = round(pay, 2)
+    print(F"each person should pay: ${pay}")
+
+
 # now we wanna get bill and percent and number of people
 print("welcome to the tip calculator")
 bill = float(input("what was the total bill? $ "))
 percent = int(input("what percentage would you like to give? 10, 12, 15? "))
 per = int(input(" How many people to split the bill? "))
 
-""" formula = (bill/per)*1.percent"""
-""" 1.percent is for paying money of food and tip (1 for food) and (0.tip  for tip) """\
-calculate = ((percent/100)+1)
-pay = (bill/per)*calculate
-pay = round(pay, 2)
-print(F"each person should pay: ${pay}")
