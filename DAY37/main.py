e75e7b66bef688cbd2933dd7f7cd37a3c7d7866b
saeed <77902443+saeedshiranii@users.noreply.github.com>
2022-01-30 17:40:42 +0330

    Day37 Completed.

--- DAY37/main.py
+++ DAY37/main.py
@@ -1,4 +1,4 @@
-import os
+from datetime import datetime
 import requests
 
 
@@ -15,8 +15,8 @@ parameter = {
     "notMinor": "yes"
 }
 
-answer = requests.post(url=user_endpoint, json=parameter)
-print(answer.text)
+#answer = requests.post(url=user_endpoint, json=parameter)
+#print(answer.text)
 
 """ now we are gonna pick a graph """
 graph_endpoint = f"{user_endpoint}/{user_name}/graphs"
@@ -30,8 +30,21 @@ graph_parameter = {
     "color": "ajisai"
 }
 
-graph = requests.post(url=graph_endpoint, json=graph_parameter, headers=headers)
-print(graph.text)
+#graph = requests.post(url=graph_endpoint, json=graph_parameter, headers=headers)
+#print(graph.text)
 
 """ for reach your graph search this address in your browser 'https://pixe.la/v1/users/shiranii/graphs/graph1.html'  """
 
+
+today = datetime.now()
+print(today.strftime("%Y%m%d"))
+draw_params= {
+            "date": today.strftime("%Y%m%d"),
+            "quantity":"40000"}
+        
+draw_url = F"{user_endpoint}/{user_name}/graphs/graph1"
+
+headers={'X-USER-TOKEN':my_token}
+
+#graph = requests.post(url=draw_url, json=draw_params, headers=headers)
+#print(graph.text)
