19f48c50f46ddac85d4af2ca9692127085d863ef
saeed <77902443+saeedshiranii@users.noreply.github.com>
2022-02-01 13:37:29 +0330

    DAY38 Completed.

--- DAY38/main.py
+++ DAY38/main.py
@@ -1,3 +1,5 @@
+from email import header
+from datetime import datetime
 import json
 import requests
 
@@ -6,6 +8,7 @@ import requests
 APP_ID = "465f8448"
 APP_KEYS = "2398f86ba20f9a6c338ba78806d3060b"
 NUTRI_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEET_ENDPOINT = "https://api.sheety.co/48f940685b4d46591451697f484b9079/myworkout/sheet1"
 user_answer = input("how many exercise you did to day?")
 
 
@@ -25,4 +28,36 @@ api_header = {
 
 respuance = requests.post(url=NUTRI_API_ENDPOINT, json = api_parameters , headers= api_header)
 user_result = respuance.json()
-print(user_result)
+
+
+today = datetime.now()
+date = f"{today.strftime('%d')}\{today.strftime('%m')}\{today.strftime('%Y')}"
+time = today.strftime('%X')
+
+
+for exercise in user_result["exercises"]:
+    workouts = {
+        "sheet1":{
+        "calories":exercise['nf_calories'],
+        "duration":exercise['duration_min'],
+        "exercise":exercise['name'].title(),
+        "time":time,
+        "date":date
+        }}
+
+
+sheet_header = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer lkjhgfdssdfghjkiuytrertyuiiuytre748596",
+}
+
+sheet_respuance = requests.post(url=SHEET_ENDPOINT, json= workouts, headers=sheet_header)
+print(sheet_respuance.text)
+
+
+
+
+
+
+
+
