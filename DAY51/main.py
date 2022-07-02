10caf7823e6e0e8c60abaa60c37f9ab29c34bb93
saeedshiranii <saeedshirani7878@.com>
2022-07-02 12:53:54 +0430

    DAY51 Done.

--- DAY51/main.py
+++ DAY51/main.py
@@ -1,49 +1,102 @@
 # a project which get a speedtest of your interrnet connection and tweet it if that is low
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
 
-# internet speed of contract
-DOWNLOAD_SPEED_CONTRACT = 1000000000
-UPLOAD_SPEED_CONTRACT = 10000000
+# The upload and download speed mentioned in the contract
+PROMISED_DOWN = 200
+PROMISED_UP = 50
+
+# email and pass of bot twitter account
 TWITTER_EMAIL = "kiramtoinmamlekat2@gmail.com"
-TWITTER_PASS = "internettestbot"
+TWITTER_PASSWORD = "kirtotwitter"
+
+
+# a class for create several web driver in separate pages
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.up = 0
+        self.down = 0
+
+
+    # get internet speed
+    def get_internet_speed(self):
+
+        self.driver.get("https://www.speedtest.net/")
+        self.driver.find_element(by=By.CLASS_NAME, value="start-text").click()
+        time.sleep(90) # Low internet speed
+        self.down = self.driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span").text
+        self.up = self.driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span").text
 
 
 
-driver = webdriver.Chrome()
-driver.get("https://www.speedtest.net/")
+    # login and tweet
+    def tweet_at_provider(self):
+        print("88")
+        self.driver.get("https://twitter.com/")
+        print("888")
+        time.sleep(40)
+        
+        # click on sing in button
+        self.driver.find_element(by=By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div').click()
 
-# click on Go button
-driver.find_element(by=By.CLASS_NAME, value="start-text").click()
+        # enter email
+        time.sleep(40)
+        email_box = self.driver.find_element(by=By.NAME, value='text')
+        email_box.send_keys(TWITTER_EMAIL)
 
-# give 1 min time to programme to test the internet speed
-time.sleep(60)
-
-# get data of internet speed
-download_speed = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span").text
-upload_speed = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span").text
-
-print(download_speed)
-print(upload_speed)
+        # click on next 
+        time.sleep(30)             
+        self.driver.find_element_by_xpath("//*[contains(text(), 'Next')]").click()
 
 
-# Now we have speed of internet we should check speed of internet with speed of our Contract
-if float(download_speed) < DOWNLOAD_SPEED_CONTRACT or float(upload_speed) < UPLOAD_SPEED_CONTRACT:
 
-    driver.get("https://twitter.com/home?lang=en")
-    #login_box_twitter = driver.find_element(by=By.CLASS_NAME, value="css-1dbjc4n r-1ets6dv r-z2wwpe r-rs99b7 r-18u37iz")
-    #login_box_twitter.send_keys(TWITTER_EMAIL)
+        try:
+            time.sleep(30)
+            usrbox = self.driver.find_element(by=By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')   
+            usrbox.send_keys("internettestbot")        
+            usrbox.send_keys(Keys.ENTER)    
 
-    email_element = driver.find_element_by_name('session[username_or_email]')
+        except:
+            pass
 
-    # PasswordElement = driver.find_element_by_name('session[password]')
+        try:
+            time.sleep(30)
+            pass_box = self.driver.find_element(by=By.XPATH, value="//input[@type='password']")
+            pass_box.send_keys(TWITTER_PASSWORD)
+            pass_box.send_keys(Keys.ENTER)
 
-    email_element.send_keys(TWITTER_EMAIL)
+        except:
+            pass
 
-    # PasswordElement.send_keys('password_goes_here')
+
+        # send twitte
+        time.sleep(5)
+        tweet_compose_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div'
+        tweet_compose = self.driver.find_element_by_xpath(tweet_compose_xpath)
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span'
+        tweet_button = self.driver.find_element_by_xpath(tweet_button_xpath)
+        tweet_button.click()
+
+
+# now start measurement
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+
+print(bot.up, bot.down)
+
+if float(bot.down) < PROMISED_DOWN or float(bot.up) > PROMISED_UP:
+    
+    bot.tweet_at_provider()
 
 
 
